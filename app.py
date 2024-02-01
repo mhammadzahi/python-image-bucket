669d7207705a53ada2a10bc8a69c47624a364d50
Mohamed Zahi <mohamedzahi33@gmail.com>
2024-02-01 16:07:28 +0400

    viws1087

--- app.py
+++ app.py
@@ -1,30 +1,60 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_session import Session
+from datetime import date, timedelta
 import os
 
 app = Flask(__name__)
-UPLOAD_FOLDER = 'uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = "zx5qsEyijgsjOhLrA7u"
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_FILE_DIR'] = '/tmp/flask_session'
+app.config['SESSION_PERMANENT'] = False 
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)
+Session(app)
+
+@app.before_request
+def redirect_https():
+    if os.environ.get('DYNO'):
+        if request.headers.get('X-Forwarded-Proto') == 'http':
+            url = request.url.replace('http://', 'https://', 1)
+            return redirect(url, code=301)
+        
+
 
 @app.route('/')
 def index():
-    # List all files in the uploads folder
     image_files = [f for f in os.listdir(app.config['UPLOAD_FOLDER']) if os.path.isfile(os.path.join(app.config['UPLOAD_FOLDER'], f))]
     return render_template('index.html', image_files=image_files)
 
+
 @app.route('/upload', methods=['POST'])
 def upload():
-    if 'file' not in request.files:
-        return redirect(request.url)
+    if request.method == 'POST':
+        print(request.url)
 
-    file = request.files['file']
+        if 'image' not in request.files:
+            return redirect(request.url)#GET
 
-    if file.filename == '':
-        return redirect(request.url)
+        file = request.files['image']
 
-    if file:
-        filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-        file.save(filename)
-        return redirect(url_for('index'))
+        if file.filename == '':
+            return redirect(request.url)
+
+        if file:
+            filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(filename)
+            return redirect(url_for('index'))
+    else:#get
+        return redirect(url_for('page_not_found'))
+
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('error.html')
 
 if __name__ == '__main__':
+    #app.run(port=8181, host="0.0.0.0")
     app.run(debug=True)
+    #app.run()
